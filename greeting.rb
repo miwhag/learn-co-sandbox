864775d756b358e760a4bd9642a69afe610baf13
Miwha Geschwind <miwhag@gmail.com>
2020-01-07 16:09:12 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,16 @@
+def greeting
+  puts "Hello World"
+end
+
+def greeting_five_times 
+  puts 
+    greeting
+    greeting
+    greeting
+    greeting
+    greeting
+end
+
+greeting_five_times
+  
+  
---
+++ learning-comments.rb
@@ -0,0 +1,20 @@
+# puts "hello world"
+# # hello this is a comment 
+
+# run_code_inside = false
+# puts "Code before if...end"
+# if run_code_inside
+#   puts "code inside"
+# end
+# puts "Code after if...end"
+
+# chance_of_rain = -23
+# if chance_of_rain <= 0.25
+#   puts "Pack a sun shelter!"
+# elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75)
+#   puts "Pack an umbrella!"
+# else
+#   puts "Stay home and read Hegel."
+# end
+
+puts Time.now.datetime_select
